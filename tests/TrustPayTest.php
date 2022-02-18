44886b8362ba1ce8e5538417e54a87588cc3e6e0
Vojtěch Bojda <info@vojtechbojda.cz>
2022-02-18 17:08:30 +0100

    fixes test

--- tests/TrustPayTest.php
+++ tests/TrustPayTest.php
@@ -6,7 +6,7 @@ use TrustPay\TrustPayPayment;
 
 final class TrustPayTest extends TestCase
 {
-    public function testBuildCardPaymentUrl(): void
+    public function testBuildBankPaymentUrl(): void
     {
         $expected = 'https://amapi.trustpay.eu/mapi5/wire/paypopup?AccountId=1&Amount=6.00&Currency=EUR&Reference=1&NotificationUrl=https%3A%2F%2Fwebsite.com%2Fnotification&PaymentType=0&Signature=ABDF4A889E8C0D0DB297DE0A4DD6EF0BA59C405C4B7DD2A132EE5119E9D60DBB';
 
